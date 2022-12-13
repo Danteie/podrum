6444fba8256d032b1a9c1b9f75f7e7c282046c76
Danteie <aleksandar.djuratovic93@gmail.com>
2022-12-13 14:20:36 +0100

    styling

--- src/Commponent/Menu/Menu.js
+++ src/Commponent/Menu/Menu.js
@@ -1,9 +1,12 @@
-
+import './Menu.css'
 
 export default function Menu() {
   return (
-    <div className="App">
-      Main Menu
+    <div className='center'>
+      <div className='center menu'>
+          ПОДРУМ Делић
+      </div>
+      
     </div>
   );
 }
