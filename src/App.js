d8f30fd2c5a5b0bdb57a5f8c96f7baf9c0c585e8
Danteie <aleksandar.djuratovic93@gmail.com>
2022-12-14 15:43:05 +0100

    adding About commponent

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Menu from './Commponent/Menu/Menu'
+import About from './Commponent/About/About';
 
 function App() {
   return (
     <div className="App">
-      <Menu />
+      <div className='landing-page'>
+        <Menu />
+      </div>
+        <About />
     </div>
   );
 }
---
+++ src/Commponent/About/About.js
@@ -0,0 +1,17 @@
+import './About.css'
+import image1 from '../../Assets/1.jpg'
+
+export default function About() {
+  return (
+    <div className="about-c">
+      <div>
+          <img src={image1} alt='vino' className='vino'/>
+      </div>
+      <div>
+          <h1>Naslov</h1>
+          <p>"Ja sam iz hobija kupio parcelu, odnosno vinograd u Petrovom polju, kod manastira Petropavlovskog, gdje je bila idealna antička lokacija. Vinograd se prostire na prostoru od hektara, sa 5.500 čokota vinove loze. Njegujemo 6, 7 velikih sorti - od autohtonih sorti - vranca i žilavke, preko francuskih internacionalnih sorti Cabernet, Sauvignon, Merlot, Chardonnay, a posljednjih godina imamo i bijelo, tamjaniku"</p>
+      </div>
+      
+    </div>
+  );
+}
